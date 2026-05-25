2eb3fabd48a4f261ea9a7fbe193ba17442e35e2a
kaif00092 <kaifarshad07@gmail.com>
2026-05-25 14:01:23 +0530

    dist folder

--- frontend/context/UserContext.jsx
+++ frontend/context/UserContext.jsx
@@ -3,11 +3,7 @@ import { createContext } from "react";
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-<<<<<<< HEAD
-  const serverUrl = import.meta.env.VITE_API_URL || "http://localhost:4001";
-=======
   const serverUrl = "https://medicare-backend-l1op.onrender.com";
->>>>>>> 2e51eff68e3be7dc09944cb4d7bd425b91e25173
 
   const value = { serverUrl };
   return (
