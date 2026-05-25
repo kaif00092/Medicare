c00245498a38466a3db9f999f454ff96071fe5cf
kaif00092 <kaifarshad07@gmail.com>
2026-05-25 11:59:28 +0530

    dist folder

--- frontend/context/UserContext.jsx
+++ frontend/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:4001";
+  const serverUrl = import.meta.env.VITE_API_URL || "http://localhost:4001";
 
   const value = { serverUrl };
   return (
